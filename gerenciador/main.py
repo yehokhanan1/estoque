93253994aa64620fbbfc637689645b5772ae96d9
Yehokhanan Oliveira <yehokhanan10@gmail.com>
2022-01-08 14:25:20 -0300

    Update main.py

--- gerenciador/main.py
+++ gerenciador/main.py
@@ -6,20 +6,24 @@ import verificar
 import adicionar
 import retirar
 
-janela = Tk()
-janela.title("Estoque")
-janela.geometry("280x300")
-janela.configure(background="#001")
-janela.wm_resizable(width=False, height=False)
-
-adProduto = Button(janela, text="Adicionar novo produto ", background="#003", foreground="#dde",command=newPro.adicionarPro).place(x=15,y=30, width=250, height=25)
-
-verProduto = Button(janela, text="Verficar produtos ", background="#003", foreground="#dde", command=verificar.verificarPro).place(x=15, y=70, width=250, height=25)
-
-addProdutos = Button(janela, text="Adicionar produto existente", background="#003", foreground="#dde", command=adicionar.addProduto).place(x=15, y=110, width=250, height=25)
-
-retirarPro = Button(janela, text="Retirar produto existente", background="#003", foreground="#dde", command=retirar.retirarPro).place(x=15, y=150, width=250, height=25)
-
-excluirPro = Button(janela, text="Excluir produto existente", background="#003", foreground="#dde", command=delet.deletPro).place(x=15, y=190, width=250, height=25)
-
-janela.mainloop()
+class windonws():
+    def __init__(self):
+        self.janela = Tk()
+        self.janela.title("Estoque")
+        self.janela.geometry("280x300")
+        self.janela.configure(background="#001")
+        self.janela.wm_resizable(width=False, height=False)
+    def buttons(self):
+        adProduto = Button(self.janela, text="Adicionar novo produto ", background="#003", foreground="#dde",command=newPro.adicionarPro)
+        adProduto.place(x=15,y=30, width=250, height=25)
+        verProduto = Button(self.janela, text="Verficar produtos ", background="#003", foreground="#dde", command=verificar.verificarPro)
+        verProduto.place(x=15, y=70, width=250, height=25)
+        addProdutos = Button(self.janela, text="Adicionar produto existente", background="#003", foreground="#dde", command=adicionar.addProduto)
+        addProdutos.place(x=15, y=110, width=250, height=25)
+        retirarPro = Button(self.janela, text="Retirar produto existente", background="#003", foreground="#dde", command=retirar.retirarPro)
+        retirarPro.place(x=15, y=150, width=250, height=25)
+        excluirPro = Button(self.janela, text="Excluir produto existente", background="#003", foreground="#dde", command=delet.deletPro)
+        excluirPro.place(x=15, y=190, width=250, height=25)
+        self.janela.mainloop()
+start = windonws()
+start.buttons()
